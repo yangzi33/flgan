ab6cdd0eaf91389ce66adb05c5e066f0acb912d0
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2022-04-28 16:03:24 -0400

    images for gan and dcgan

--- lsgan-fashion-mnist.py
+++ lsgan-fashion-mnist.py
@@ -1,5 +1,4 @@
 import argparse
-from cmath import tanh
 import os
 import numpy as np
 import math
@@ -24,11 +23,10 @@ parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rat
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=1024, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=153, help="size of each image dimension")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=100, help="number of image channels")
-parser.add_argument("--num_class", type=int, default=37, help="number of class")
+parser.add_argument("--sample_interval", type=int, default=1000, help="number of image channels")
 opt = parser.parse_args()
 print(opt)
 
@@ -48,36 +46,25 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        # self.init_size = opt.img_size // 4
-        self.init_size = 7
-        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 256 * self.init_size ** 2))
+        self.init_size = opt.img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
 
         self.conv_blocks = nn.Sequential(
-            nn.ConvTranspose2d(256, 256, 3, stride=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(256, 256, 3, stride=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(256, 256, 3, stride=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(256, 256, 3, stride=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(256, 128, 3, stride=2),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(128, 64, 3, stride=2),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(64, opt.channels, 3, stride=1),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
             nn.Tanh(),
         )
 
     def forward(self, z):
         out = self.l1(z)
-        out = out.view(out.shape[0], 256, self.init_size, self.init_size)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
         img = self.conv_blocks(out)
         return img
 
@@ -87,22 +74,21 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         def discriminator_block(in_filters, out_filters, bn=True):
-            block = [nn.Conv2d(in_filters, out_filters, 5, stride=2), nn.LeakyReLU(inplace=True), nn.Dropout2d(0.25)]
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
             if bn:
-                block.append(nn.BatchNorm2d(out_filters))
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
             return block
 
         self.model = nn.Sequential(
-            *discriminator_block(opt.channels, 64, bn=False),
+            *discriminator_block(opt.channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
             *discriminator_block(64, 128),
-            *discriminator_block(128, 256),
-            *discriminator_block(256, 512),
         )
 
         # The height and width of downsampled image
-        #ds_size = opt.img_size // 2 ** 4
-        ds_size = 6
-        self.adv_layer = nn.Linear(512 * ds_size ** 2, 1)
+        ds_size = opt.img_size // 2 ** 4
+        self.adv_layer = nn.Linear(128 * ds_size ** 2, 1)
 
     def forward(self, img):
         out = self.model(img)
@@ -129,14 +115,14 @@ generator.apply(weights_init_normal)
 discriminator.apply(weights_init_normal)
 
 # Configure data loader
-os.makedirs("../../data/fasionMNIST", exist_ok=True)
+os.makedirs("../../data/cifar10", exist_ok=True)
 dataloader = torch.utils.data.DataLoader(
-    datasets.FashionMNIST(
-        "../../data/fashionMNIST",
-        #train=True,
+    datasets.CIFAR10(
+        "../../data/cifar10",
+        train=True,
         download=True,
         transform=transforms.Compose(
-            [transforms.Resize((opt.img_size,opt.img_size)), transforms.ToTensor(), transforms.Normalize([0.5], [0.5], [0.5])]
+            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         ),
     ),
     batch_size=opt.batch_size,
@@ -154,9 +140,7 @@ Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 # ----------
 
 for epoch in range(opt.n_epochs):
-    for i, (imgs, labels) in enumerate(dataloader):
-
-        labels = nn.functional.one_hot(labels, num_classes=opt.num_class)
+    for i, (imgs, _) in enumerate(dataloader):
 
         # Adversarial ground truths
         valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
